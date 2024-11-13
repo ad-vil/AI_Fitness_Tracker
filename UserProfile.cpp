36fce0d4f269b9612fceddd3f2a34f739b901557
Adil Siddiqui <adil_siddiki@hotmail.com>
2024-11-13 12:29:08 +0500

    making userProfile class

--- UserProfile.cpp
+++ UserProfile.cpp
@@ -4,6 +4,22 @@
 
 #include "UserProfile.h"
 
-int main() {
+// constructor init user profile attributes
+UserProfile::UserProfile(const string& name, int age, double weight, double height, const string& goal, const string& gender)
+    : name(name), age(age), weight(weight), height(height), goal(goal), gender(gender) {}
 
+// updates height and weight in user profile
+void UserProfile::updateProfile(double newWeight, double newHeight) {
+    weight = newWeight;
+    height = newHeight;
+}
+
+// displays profile details
+void UserProfile::displayProfile() const {
+    cout << "name: " << name << endl;
+    cout << "age: " << age << endl;
+    cout << "weight: " << weight << " lbs" << endl;
+    cout << "height: " << height << " inches" << endl;
+    cout << "goal: " << goal << endl;
+    cout << "gender: " << gender << endl;
 }
--- UserProfile.h
+++ UserProfile.h
@@ -5,12 +5,29 @@
 #ifndef USERPROFILE_H
 #define USERPROFILE_H
 
+#include <string>
+#include <iostream>
 
+using namespace std;
 
 class UserProfile {
+private:
+    string name;
+    int age;
+    double weight; // in lbs
+    double height; // in inches
+    string goal; // e.g., "weight loss" or "muscle gain"
+    string gender;
 
+public:
+    // constructor
+    UserProfile(const string &name, int age, double weight, double height, const string &goal, const string &gender);
+
+    // updates user profile with new weight and height
+    void updateProfile(double newWeight, double newHeight);
+
+    // displays user profile details
+    void displayProfile() const;
 };
 
-
-
-#endif //USERPROFILE_H
+#endif // USERPROFILE_H
